7ad1cb4feabdda069deedd450d3f8e2d9c86e421
Kerian <kerian.gouriou@gmail.com>
2026-04-06 19:56:22 +0200

    FP Controls Version 1
    I added a simple First Person control system with a little speed boost after each jump to make the game more dynamic and some control over your jump size to add a bit of depth in the gameplay.

---
+++ Emergency Protocol/Assets/Script/CameraScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Emergency Protocol/Assets/Script/PlayerController.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    public Transform cameraPivot;
+
+    private CharacterController controller;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private bool jumpInput;
+    public bool isJumping;
+    private int jumpBoostFrame;
+    public int jumpBoostLenght = 40;
+    public float jumpBoostStrenght = 1.5f;
+
+    private float yVelocity;
+    private float xRotation = 0f;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Jump();
+        HandleMovement();
+        HandleLook();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumpInput = true;
+        }
+    }
+    public void OnStopJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumpInput = false;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        if (controller.isGrounded && yVelocity < 0)
+            yVelocity = -2f;
+
+        if (jumpInput == false)
+        {
+            yVelocity += -20f * Time.deltaTime;
+        }
+        else
+        {
+            yVelocity += -13f * Time.deltaTime;
+        }
+
+        Vector3 velocity = move * speed;
+        if(jumpBoostFrame>0)
+        {
+            velocity = velocity * (1f + jumpBoostStrenght - (jumpBoostStrenght - jumpBoostStrenght * 0.05f * jumpBoostFrame));
+            jumpBoostFrame --;
+        }
+        velocity.y = yVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void HandleLook()
+    {
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        cameraPivot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void Jump()
+    {
+        if (controller.isGrounded && jumpInput == true)
+        {
+            isJumping = true;
+            jumpBoostFrame = jumpBoostLenght;
+            yVelocity = 8f;
+        }
+
+        if(isJumping == true && yVelocity<0)
+        {
+            isJumping = false;
+        }
+    }
+}
