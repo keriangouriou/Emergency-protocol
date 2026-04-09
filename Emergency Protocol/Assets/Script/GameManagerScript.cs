de8dcadf03f2a0513d9635cfa98db88cc41b9191
Kerian <kerian.gouriou@gmail.com>
2026-04-09 23:05:38 +0200

    Level Main Layout Has been Added

--- Emergency Protocol/Assets/Script/GameManagerScript.cs
+++ Emergency Protocol/Assets/Script/GameManagerScript.cs
@@ -6,19 +6,26 @@ public class GameManagerScript : MonoBehaviour
     private PlayerControllerFP playerControllerFP;
     private PlayerControllerSide playerControllerSide;
     private ScreenCenterMovement screenCenterMovement;
+    private GameObject deathScreen;
 
     private void Awake()
     {
         playerControllerFP = GameObject.Find("Character").GetComponent<PlayerControllerFP>();
         playerControllerSide = GameObject.Find("Character").GetComponent<PlayerControllerSide>();
         screenCenterMovement = GameObject.Find("ScreenCenter").GetComponent <ScreenCenterMovement>();
+        deathScreen = GameObject.Find("DeathScreen");
+        deathScreen.SetActive(false);
     }
     public void Lose()
     {
         Debug.Log("You lose");
+        TurnOff();
+        deathScreen.SetActive(true);
+    }
+    public void Restart()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
     private void TurnOff()
     {
         playerControllerFP.enabled = false;
