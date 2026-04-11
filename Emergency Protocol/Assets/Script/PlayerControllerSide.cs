e332041bce043c2cb8563cc122e1bae209ae38c7
Kerian <kerian.gouriou@gmail.com>
2026-04-11 18:48:50 +0200

    Clamp Added In Side Mode

--- Emergency Protocol/Assets/Script/PlayerControllerSide.cs
+++ Emergency Protocol/Assets/Script/PlayerControllerSide.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,7 +11,15 @@ public class PlayerControllerSide : MonoBehaviour
     private Vector2 moveInput;
     private float speed = 15f;
     private float screenSpeed;
-    private Transform screenCenterTransform;
+    private float distanceZ;
+    private float distanceZNoAbsolute;
+    private float distanceY;
+    private float distanceYNoAbsolute;
+    private float LimitZ;
+    private float LimitY;
+    private float ClampZ = 12f;
+    private float ClampY = 6.5f;
+    public Transform screenCenterTransform;
 
     private void Awake()
     {
@@ -29,10 +40,33 @@ public class PlayerControllerSide : MonoBehaviour
     }
     private void HandleMovement()
     {
+        //Store inputs direction into a Vector 3
         Vector3 move = transform.forward * moveInput.x + transform.up * moveInput.y;
-        
-        Vector3 velocity = move * speed + transform.forward * screenSpeed;
 
+        //Clamp Z
+        distanceZNoAbsolute = transform.position.z - screenCenterTransform.position.z;
+        distanceZ = Math.Abs(distanceZNoAbsolute);
+        LimitZ = ClampZ - distanceZ;
+        if (LimitZ <= 0.2 && Math.Sign(move.z) == Math.Sign(distanceZNoAbsolute))
+        {
+            move.z = 0;
+            Vector3 clampZ = transform.forward * (screenCenterTransform.position.z + ClampZ * Math.Sign(distanceZNoAbsolute) - transform.position.z);
+            controller.Move(clampZ);
+        }
+
+        //Clamp Y
+        distanceYNoAbsolute = transform.position.y - screenCenterTransform.position.y;
+        distanceY = Math.Abs(distanceYNoAbsolute);
+        LimitY = ClampY - distanceY;
+        if (LimitY <= 0.2 && Math.Sign(move.y) == Math.Sign(distanceYNoAbsolute))
+        {
+            move.y = 0;
+            Vector3 clampY = transform.up * (screenCenterTransform.position.y + ClampY * Math.Sign(distanceYNoAbsolute) - transform.position.y);
+            controller.Move(clampY);
+        }
+
+        //Apply speed multiplayer and screenspeed then move
+        Vector3 velocity = move * speed + transform.forward * screenSpeed;
         controller.Move(velocity * Time.deltaTime);
     }
 }
